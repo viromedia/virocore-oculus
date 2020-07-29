5d1b70262c6d877b3f6ed148e80879e4f81295b3
Daniel Thian <daniel@viromedia.com>
2020-07-28 19:07:08 -0700

    Added Sticky Reticle

--- ViroRenderer/VROInputPresenter.h
+++ ViroRenderer/VROInputPresenter.h
@@ -140,6 +140,17 @@ public:
         _camera = std::make_shared<VROCamera>(camera);
     }
 
+    void setStickyReticleDepth(bool sticky) {
+        _reticleStickyDepth = sticky;
+    }
+
+    void setForcedRender(bool forced) {
+        _forcedRender = forced;
+    }
+
+    bool getForcedRender() {
+        return _forcedRender;
+    }
 protected:
     std::shared_ptr<VROCamera> _camera;
     std::shared_ptr<VRONode> _rootNode;
@@ -155,7 +166,7 @@ protected:
 
         // **Experimental**
         // For reticle behavior that can be occluded and is in the world.
-        if (!_reticles[reticleId]->isHudBased()) {
+        if (!_reticles[reticleId]->isHudBased() && !_reticleStickyDepth) {
             if (event.isBackgroundHit) {
                 VROVector3f proj = (event.intersecPos - controllerPos).normalize();
                 recticlePosition = controllerPos + (proj.scale(16));
@@ -169,11 +180,30 @@ protected:
             float worldPerScreen = _camera->getWorldPerScreen(depth);
             float radius = fabs(worldPerScreen) * kReticleSizeMultiple;
             _reticles[reticleId]->setRadius(radius);
-            _reticleInitialPositionSet = true;
 
             if (!_reticles[reticleId]->isHeadlocked() && _camera != nullptr) {
                 _reticles[reticleId]->setPosition(recticlePosition);
             }
+
+        } else if (!_reticles[reticleId]->isHudBased()) {
+            float posDepth = event.intersecPos.distance(controllerPos);
+            posDepth = VROMathClamp(posDepth, 0.5, INFINITY);
+            depth = -(posDepth);
+
+            if (!event.isBackgroundHit || kDebugSceneBackgroundDistance) {
+                _reticles[reticleId]->setLastKnownDistance(-depth);
+            }
+
+            float lastKnownDistance = _reticles[reticleId]->getLastKnownDistance();
+            if (!_reticles[reticleId]->isHeadlocked() && _camera != nullptr) {
+                VROVector3f proj = (event.intersecPos - controllerPos).normalize();
+                VROVector3f finalPos = controllerPos + proj.scale(lastKnownDistance);
+                _reticles[reticleId]->setPosition(finalPos);
+            }
+
+            float worldPerScreen = _camera->getWorldPerScreen(lastKnownDistance);
+            float radius = fabs(worldPerScreen) * kReticleSizeMultiple;
+            _reticles[reticleId]->setRadius(radius);
         } else if (!_reticles[reticleId]->isHeadlocked()) {
             depth = VROMathClamp(depth, 1.0, INFINITY);
             float worldPerScreen = _camera->getWorldPerScreen(depth);
@@ -213,6 +243,14 @@ private:
     std::map<int, std::shared_ptr<VROReticle>> _reticles;
     bool _reticleInitialPositionSet;
     VROAtomic<VROVector3f> _lastKnownForward;
+    bool _reticleStickyDepth = true;
+
+    /*
+     If true, the controller is re-rendered last after a complete render pass. This is
+     a mitigation for ensuring that controllers are rendered irregardless of which portal
+     they are in (for cases where the controllers are trasversing across portals).
+     */
+    bool _forcedRender = false;
 
     /*
      Event delegate for triggering calls back to Controller_JNI.
